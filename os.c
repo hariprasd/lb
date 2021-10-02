bb773d2c44aaad0dd186d6f79a296b0ad767de2a
Hari Prasad <75234157+hariprasd@users.noreply.github.com>
2021-10-02 08:46:04 +0530

    Update os.c

--- os.c
+++ os.c
@@ -1,3 +1,183 @@
+Banker :
+ #include <stdio.h>
+int main()
+{
+  
+    int n, m, i, j, k;
+    n = 5; 
+    m = 3; 
+    int alloc[5][3] = { { 0, 1, 0 }, 
+                        { 2, 0, 0 }, 
+                        { 3, 0, 2 }, 
+                        { 2, 1, 1 }, 
+                        { 0, 0, 2 } }; 
+  
+    int max[5][3] = { { 7, 5, 3 }, 
+                      { 3, 2, 2 }, 
+                      { 9, 0, 2 }, 
+                      { 2, 2, 2 }, 
+                      { 4, 3, 3 } }; 
+    int avail[3] = { 3, 3, 2 }; 
+    int f[n], ans[n], ind = 0;
+    for (k = 0; k < n; k++) {
+        f[k] = 0;
+    }
+    int need[n][m];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+    }
+    int y = 0;
+    for (k = 0; k < 5; k++) {
+        for (i = 0; i < n; i++) {
+            if (f[i] == 0) {
+  
+                int flag = 0;
+                for (j = 0; j < m; j++) {
+                    if (need[i][j] > avail[j]){
+                        flag = 1;
+                         break;
+                    }
+                }
+  
+                if (flag == 0) {
+                    ans[ind++] = i;
+                    for (y = 0; y < m; y++)
+                        avail[y] += alloc[i][y];
+                    f[i] = 1;
+                }
+            }
+        }
+    }
+  
+    printf("Following is the SAFE Sequence\n");
+    for (i = 0; i < n - 1; i++)
+        printf(" P%d ->", ans[i]);
+    printf(" P%d", ans[n - 1]);
+    printf("\n\n");
+  
+    return (0);
+  
+
+}
+  
+
+Bestfit:
+
+#include<stdio.h>
+#define MAX 20
+int main()
+{
+int bsize[MAX],fsize[MAX],nb,nf;
+int temp,low=10000;
+static int bflag[MAX],fflag[MAX];
+int i,j;
+printf("\n enter the number of blocks");
+scanf("%d",&nb);
+for(i=1;i<=nb;i++)
+{
+printf("Enter the size of memory block % d",i);
+scanf("%d", &bsize[i]);
+}
+printf("\n enter the number of files");
+scanf("%d",&nf);
+for(i=1;i<=nf;i++)
+{
+printf("\n enter the size of file %d",i);
+scanf("%d",&fsize[i]);
+}
+for(i=1;i<=nf;i++)
+{
+for(j=1;j<=nb;j++)
+{
+if(bflag[j]!=1)
+{
+temp=bsize[j]-fsize[i];
+if(temp>=0)
+{
+if(low>temp)
+{
+fflag[i]=j;
+low=temp;
+}
+}
+}}
+bflag[fflag[i]]=1;
+low=10000;
+}
+printf("\n file no \t file.size\t block no \t block size");
+for(i=1;i<=nf;i++)
+printf("\n \n %d \t\t%d\t\t%d\t\t%d",i,fsize[i],fflag[i],bsize[fflag[i]]);
+}
+
+
+FirstFit :
+
+#include<stdio.h>
+struct process
+{
+int ps;
+int flag;
+} p[50];
+struct sizes
+{
+int size;
+int alloc;
+}
+s[5];
+int main()
+{
+int i=0,np=0,n=0,j=0;
+printf("\n first fit");
+printf("\n");
+printf("enter the number of blocks \t");
+scanf("%d",&n);
+printf("\t\t\n enter the size for %d blocks\n",n);
+for(i=0;i<n;i++)
+{
+printf("enter the size for %d block \t",i);
+scanf("%d",&s[i].size);
+}
+printf("\n\t\t enter the number of process\t",i);
+scanf("%d",&np);
+printf("enter the size of %d processors !\t",np);
+printf("\n");
+for(i=0;i<np;i++)
+{
+printf("enter the size of process %d\t",i);
+scanf("\n%d",&p[i].ps);
+}
+printf("\n\t\t Allocation of blocks using first fit is as follows\n");
+printf("\n\t\t process \t process size\t blocks\n");
+for(i=0;i<np;i++)
+{
+for(j=0;j<n;j++)
+{
+if(p[i].flag!=1)
+{
+if(p[i].flag!=1)
+{
+if(p[i].ps<=s[j].size)
+{
+if(!s[j].alloc)
+{
+p[i].flag=1;
+s[j].alloc=1;
+printf("\n\t\t\t %d\t\t\t\t%d\t\t\t%d\t",i,p[i].ps,s[j].size);
+}
+}
+}
+}
+}
+}
+for(i=0;i<np;i++)
+{
+if(p[i].flag!=1)
+printf(" \n sorry !!!!!!!process %d must wait as there is no sufficient memory",i);
+}
+}
+
+
 FCFS :
 
 #include<stdio.h>
@@ -10,7 +190,7 @@ int i;
 clrscr();
 printf("\nEnter the no.of processes \n");
 scanf("%d",&n);
-printf("Enter burst time for each process\n")
+printf("Enter burst time for each process\n");
 for(i=0;i<n;i++)
 {
 scanf("%d",&bt[i]);
@@ -49,8 +229,7 @@ printf("\nprocess[%d]\t\t%d\t\t%d\t\t%d",p[i],bt[i],tat[i],wt[i]);
 return 0;
 }
 
-
-SJF:
+SJF :
 
 #include<stdio.h>
 #include<conio.h>
@@ -126,140 +305,6 @@ return 0;
 }
 
 
-Priority Scheduling Algo :
-
-#include<stdio.h>
-#include<conio.h>
-int p[30],bt[30],tot_tat=0,pr[30],wt[30],n,tot_wt=0,tat[30],PR_wt=0,PR_tat=0;
-float awt,avg_tat,avg_wt;
-void main()
-{
-int i;
-clrscr();
-printf("\nEnter the no.of processes \n");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-printf("Enter burst time and priority of process[%d]:",i+1);
-scanf("%d%d",&bt[i],&pr[i]);
-p[i] = i;
-}
-sort();
-WT_TAT(&PR_tat,&PR_wt);
-printf("\n\nTotal Turn around Time:%d",PR_tat);
-printf("\nAverage Turn around Time :%d ", PR_tat/n);
-printf("\nTotal Waiting Time:%d",PR_wt);
-printf("\nTotal avg. Waiting Time:%d",PR_wt/n);
-getch();
-}
-int sort()
-{
-int t,i,j,t2,t1;
-for(i=0;i<n;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(pr[i]>pr[j])
-{
-swap(&bt[j],&bt[i]);
-swap(&p[j],&p[i]);
-swap(&pr[j],&pr[i]);
-}
-}
-}
-return 0;
-}
-int swap(int *a, int *b)
-{
-int t;
-t = *a;
-*a = *b;
-*b = t;
-return 0;
-}
-int WT_TAT(int *a, int *b)
-{
-int i;
-for(i=0;i<n;i++)
-{
-if(i==0)
-tat[i] = bt[i];
-else
-tat[i] = tat[i-1] + bt[i];
-tot_tat=tot_tat+tat[i];
-}
-*a = tot_tat;
-wt[0]=0;
-for(i=1;i<n;i++)
-{
-wt[i]=wt[i-1]+bt[i-1];
-tot_wt = tot_wt+wt[i];
-}
-*b = tot_wt;
-printf("\nPROCESS\t\tBURST TIME\tPRIORITY\tTURN AROUND TIME\tWAITING
-TIME");
-for(i=0; i<n; i++)
-printf("\nprocess[%d]\t\t%d\t\t%d\t\t%d\t\t%d"
-,p[i]+1,bt[i],pr[i],tat[i],wt[i]);
-return 0; }
-
-
-RoundRobin Sheduling Algo :
-
-#include<stdio.h>
-#include<conio.h>
-int TRUE = 0;
-int FALSE = -1;
-int tbt[30],bt[30],tat[30],n=0,wt[30],qt=0,tqt=0,time=0,lmore,t_tat=0,t_wt=0;
-void main()
-{
-int i,j;
-clrscr();
-printf("\nEnter no. of processors:");
-scanf("%d",&n);
-printf("\nEnter Quantum Time:");
-scanf("%d",&qt);
-for(i=0;i<n;i++)
-{
-printf("\nEnter Burst Time of Processor[%d]:",i+1);
-scanf("%d",&bt[i]);
-tbt[i] = bt[i];
-wt[i] = tat[i] = 0;
-}
-
-lmore = TRUE;
-while(lmore == TRUE)
-{
-lmore = FALSE;
-for(i=0;i<n;i++)
-{
-if(bt[i] != 0)
-wt[i] = wt[i] + (time - tat[i]);
-tqt = 1;
-while(tqt <= qt && bt[i] !=0)
-{
-lmore = TRUE;
-bt[i] = bt[i] -1;
-tqt++;
-time++;
-tat[i] = time;
-}
-}
-}
-printf("\nProcessor ID\tBurstTime\tTurnAroundTime\tWaitingTime\n");
-for(i=0;i<n;i++)
-{
-printf("Processor%d\t\t%d\t\t%d\t\t%d\n",i+1,tbt[i],tat[i],wt[i]);
-t_tat = t_tat + tat[i];
-t_wt = t_wt + wt[i];
-}
-printf("\nTotal Turn Around Time:%d",t_tat);
-printf("\nAverage Turn Around Time:%d",t_tat/n);
-printf("\nTotal Waiting Time:%d",t_wt);
-printf("\nAverage Waiting Time:%d",t_wt/n);
-getch();
-}
-
 Semaphore :
 
 #include<stdio.h>
@@ -271,8 +316,8 @@ scanf("%d",&n);
 if(0<=(buffersize-(currentsize+n)))
 {
 currentsize+=n;
-printf("%d Elements produced by producer where buffersize is %d\n", currentsize,buffersize);
-1
+printf("%d Elements produced by producer where buffersize is %d\n", currentsize,
+buffersize);
 }
 else
 printf("\nBuffer is not sufficient\n");
@@ -323,120 +368,3 @@ break;
 }
 while(c!=3);
 }
-
-
-FirstFit :
-
-#include<stdio.h>
-struct process
-{
-int ps;
-int flag;
-struct sizes
-{
-int size;
-int alloc;
-}
-s[5];
-int main()
-{
-int i=0,np=0,n=0,j=0;
-printf("\n first fit");
-printf("\n");
-printf("enter the number of blocks \t");
-scanf("%d",&n);
-printf("\t\t\n enter the size for %d blocks\n",n);
-for(i=0;i<n;i++)
-{
-printf("enter the size for %d block \t",i);
-scanf("%d",&s[i].size);
-}
-printf("\n\t\t enter the number of process\t",i);
-scanf("%d",&np);
-printf("enter the size of %d processors !\t",np);
-printf("/n");
-for(i=0;i<np;i++)
-{
-printf("enter the size of process %d\t",i);
-scanf("\n%d",&p[i].ps);
-}
-printf("\n\t\t Allocation of blocks using first fit is as follows\n");
-printf("\n\t\t process \t process size\t blocks\n");
-for(i=0;i<np;i++)
-{
-for(j=0;j<n;j++)
-{
-if(p[i].flag!=1)
-{
-if(p[i].flag!=1)
-{
-if(p[i].ps<=s[j].size)
-{
-if(!s[j].alloc)
-{
-p[i].flag=1;
-s[j].alloc=1;
-printf("\n\t\t %d\t\t\t%d\t%d\t",i,p[i].ps,s[j].size);
-}
-}
-}
-}
-}
-}
-for(i=0;i<np;i++)
-{
-if(p[i].flag!=1)
-printf("sorry !!!!!!!process %d must wait as there is no sufficient memory");
-}
-}
-
-
-BestFit :
-
-#include<stdio.h>
-#define MAX 20
-int main()
-{
-int bsize[MAX],fsize[MAX],nb,nf;
-int temp,low=10000;
-static int bflag[MAX],fflag[MAX];
-int i,j;
-printf("\n enter the number of blocks");
-scanf("%d",&nb);
-for(i=1;i<=nb;i++)
-{
-printf("Enter the size of memory block % d",i);
-scanf("%d", &bsize[i]);
-}
-printf("\n enter the number of files");
-scanf("%d",&nf);
-for(i=1;i<=nf;i++)
-{
-printf("\n enetr the size of file %d",i);
-scanf("%d",&fsize[i]);
-}
-for(i=1;i<=nf;i++)
-{
-for(j=1;j<=nb;j++)
-{
-if(bflag[j]!=1)
-{
-temp=bsize[j]-fsize[i];
-if(temp>=0)
-{
-if(low>temp)
-{
-fflag[i]=j;
-low=temp;
-}
-}
-}}
-bflag[fflag[i]]=1;
-low=10000;
-}
-printf("\n file no \t file.size\t block no \t block size");
-for(i=1;i<=nf;i++)
-printf("\n \n %d \t\t%d\t\t%d\t\t%d",i,fsize[i],fflag[i],bsize[fflag[i]]);
-}
-
-} p[50];
